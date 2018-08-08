e1ef47d34efc19a8ca6e5d161cda755fc92e53cf
maxis55 <rocketnottester@gmail.com>
2018-08-08 16:41:43 +0300

    Minimal example

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
 require ('includes/fpdf/fpdf.php');
-echo 'Hello cruel world';
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','U',16);
+$pdf->Cell(40,10,'Hello Cruel World!');
+$pdf->Output('f','test.pdf');
